cac85e3c2441a44ce34fceab3e62cf827cdb90ee
minhaz74692 <minhaz74692@gmail.com>
2023-07-20 15:11:11 +0600

    Final Commit2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import firestore from './firebase';
 import IssueTable from './components/IssueTable';
+import { v4 as uuidv4 } from "uuid";
 
 
 const App = (props) => {
+  const [id, setId] = useState('');
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [description, setDescription] = useState('');
   const [issue, setIssue] = useState('');
   const [status, setStatus] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [operation, setOperation] = useState('');
   const [updateStart, setUpdateStart] = useState(false);
 
   // Handle update and submit button visibility
-  const handleUpdateStatus =(name, email, des, issue)=>{
+  const handleUpdateStatus = (id,name, email, des, issue) => {
+    setId(id)
     setUpdateStart(true);
     setName(name);
     setEmail(email);
@@ -26,6 +31,7 @@ const App = (props) => {
 
   const issuesRef = firestore.collection('issues');
   const dataToAddorUpdate = {
+    id: uuidv4(),
     name,
     email,
     description,
@@ -34,75 +40,114 @@ const App = (props) => {
   };
   const [issues, setIssues] = useState([]);
 
+  const scrollToBottom = () => {
+    const bottomElement = document.getElementById('bottomElement');
+    bottomElement.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  const fieldClear = () => {
+    setName('');
+    setEmail('');
+    setDescription('');
+    setIssue('');
+  }
+
   // Add new Issue
   const handleAddIssue = async (e) => {
     e.preventDefault();
+    setLoading(true);
+    setOperation('...adding');
     const querySnapshot = await issuesRef.where('email', '==', email).get();
-    console.log(querySnapshot.docs.length);
-    querySnapshot.docs.length>0?window.alert('you can not add more then one issue with same email.'):
-     dataToAddorUpdate.email!=''&&dataToAddorUpdate.issue!=''? await issuesRef.add(dataToAddorUpdate)
-      .then((docRef) => {
-        console.log('Issue added: ', docRef.id);
-        window.location.reload();
-      })
-      .catch((error) => {
-        console.error('Error adding document: ', error);
-      }):window.alert('Email and Issue are Required');
-    
+
+    let isAnyDocumentWithFalseStatus = true;
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      if (data.status === false) {
+        isAnyDocumentWithFalseStatus = false;
+      }
+      // console.log(isAnyDocumentWithTrueStatus);
+    });
+    querySnapshot.docs.length > 0 && isAnyDocumentWithFalseStatus ===false ? window.alert('you can not add more then one issue with same email with pending status.') :
+      dataToAddorUpdate.email != '' && dataToAddorUpdate.issue != '' ? await issuesRef.add(dataToAddorUpdate)
+        .then((docRef) => {
+          console.log('Issue added: ', docRef.id);
+          fieldClear();
+          scrollToBottom();
+          setIssues([...issues, dataToAddorUpdate]);
+        })
+        .catch((error) => {
+          console.error('Error adding document: ', error);
+        }) : window.alert('Email and Issue are Required');
+    setLoading(false);
+
   }
 
   // Edit Issue
-  const handleEditIssue = async (email , newData) => {
+  const handleEditIssue = async (email, newData) => {
+    setLoading(true);
+    setOperation('...updating');
     try {
       const querySnapshot = await issuesRef.where('email', '==', email).get();
-  
+
       const batch = firestore.batch();
       querySnapshot.forEach((doc) => {
         batch.update(doc.ref, newData);
       });
-  
+
       await batch.commit();
-      window.alert('Issue deleted successfully.')
-        window.location.reload();
+      fieldClear();
+      setIssues(issues.map((issue) => {
+        return issue.email === email ? { ...dataToAddorUpdate } : issue;
+      }));
       console.log('Issue updated successfully.');
     } catch (error) {
       console.error('Error updating documents:', error);
     }
+    setLoading(false);
+    setUpdateStart(false);
   };
 
   // Handle Issue Status
-  const handleStatus = async (email , newData) => {
+  const handleStatus = async (id, newData) => {
+    setLoading(true);
     try {
-      const querySnapshot = await issuesRef.where('email', '==', email).get();
-  
+      const querySnapshot = await issuesRef.where('id', '==', id).get();
+
       const batch = firestore.batch();
       querySnapshot.forEach((doc) => {
         batch.update(doc.ref, newData);
       });
-  
+
       await batch.commit();
-      window.alert('Issue status changed successfully.')
-        window.location.reload();
+      
+      setIssues(issues.map((issue) => {
+        return issue.id === id ? {id: issue.id,name:issue.name, email: issue.email, description: issue.description, issue: issue.issue, status : issue.status=== true?false:true } : issue;
+      }));
     } catch (error) {
       console.error('Error updating documents:', error);
     }
+    setLoading(false);
   };
 
   // Delete Issue
-  const handleDelete = async (email) => {
+  const handleDelete = async (id) => {
+    setLoading(true);
+    setOperation('...deleting');
     try {
-      const querySnapshot = await issuesRef.where('email', '==', email).get();
+      const querySnapshot = await issuesRef.where('id', '==', id).get();
       const batch = firestore.batch();
       querySnapshot.forEach((doc) => {
         batch.delete(doc.ref);
       });
       await batch.commit();
-      window.alert('Issue deleted successfully.')
-        window.location.reload();
-      console.log('Issue deleted successfully.');
+      const newIssueList = issues.filter((issue) => {
+        return issue.id !== id;
+      });
+      setIssues(newIssueList);
     } catch (error) {
       console.error('Error deleting documents:', error);
     }
+    setLoading(false);
   };
 
   // Fetch Firestore data
@@ -122,49 +167,53 @@ const App = (props) => {
 
   return (
     <div className='container mt-2'>
-      <div  className='col-6 ms-auto me-auto'>
-      <h2 className='text-center'>Issue Tracker</h2>
-      <form>
-        <div className="mb-2">
-          <label htmlFor="name" className="form-label">Name</label>
-          <input type="text" className="form-control" id="name" aria-describedby="name" required value={name}
-            onChange={(e) => setName(e.target.value)} />
+      <div className='col-6 ms-auto me-auto'>
+        <h2 className='text-center'>Issue Tracker</h2>
+        <form>
+          <div className="mb-2">
+            <label htmlFor="name" className="form-label">Name</label>
+            <input type="text" className="form-control" id="name" aria-describedby="name" required value={name}
+              onChange={(e) => setName(e.target.value)} />
 
-        </div>
-        <div className="mb-2">
-          <label htmlFor="email" className="form-label">Email Address</label>
-          <input type="email" className="form-control" id="email" aria-describedby="email" required value={email}
-            onChange={(e) => setEmail(e.target.value)} />
-        </div>
-        <div className="mb-2">
-          <label htmlFor="issueDescription" className="form-label">Issue Description</label>
-          <input type="text" className="form-control" id="issueDescription" aria-describedby="issueDescription" required value={description}
-            onChange={(e) => setDescription(e.target.value)} />
-        </div>
+          </div>
+          <div className="mb-2">
+            <label htmlFor="email" className="form-label">Email Address</label>
+            <input type="email" className="form-control" id="email" aria-describedby="email" required value={email}
+              onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="issueDescription" className="form-label">Issue Description</label>
+            <input type="text" className="form-control" id="issueDescription" aria-describedby="issueDescription" required value={description}
+              onChange={(e) => setDescription(e.target.value)} />
+          </div>
 
-        {/* DropDown issue selection */}
-        <div className='mb-2'>
-          <label htmlFor="issue" className=''>Choose a type of issue:</label>
-          <select id="issue" name="issue" className='ms-3 rounded py-2 px-5' required value={issue}
-            onChange={(e) => setIssue(e.target.value)}>
-            <option value="select issue inactive">-Select a Issue-</option>
-            <option value="Slow Speed">Slow Speed</option>
-            <option value="Connection Drop">Connection Drop</option>
-            <option value="No Connection">No Connection</option>
-          </select>
-        </div>
+          {/* DropDown issue selection */}
+          <div className='mb-2'>
+            <label htmlFor="issue" className=''>Choose a type of issue:</label>
+            <select id="issue" name="issue" className='ms-3 rounded py-2 px-5' required value={issue}
+              onChange={(e) => setIssue(e.target.value)}>
+              <option value="select issue inactive">-Select a Issue-</option>
+              <option value="Slow Speed">Slow Speed</option>
+              <option value="Connection Drop">Connection Drop</option>
+              <option value="No Connection">No Connection</option>
+            </select>
+          </div>
 
 
-      </form>
-      <button type="submit" className="btn btn-primary" style={{'display': updateStart?'none':'block'}} onClick={handleAddIssue} >Submit</button>
+        </form>
+        <button type="submit" className="btn btn-primary" style={{ 'display': updateStart ? 'none' : 'block' }} onClick={handleAddIssue} >Submit</button>
 
-      <button type="submit" className="btn btn-primary" style={{'display': updateStart?'block':'none'}} onClick={()=>handleEditIssue(email, dataToAddorUpdate)} >Update Issue</button>
+        <button type="submit" className="btn btn-primary" style={{ 'display': updateStart ? 'block' : 'none' }} onClick={() => handleEditIssue(email, dataToAddorUpdate)} >Update Issue</button>
 
       </div>
 
+      {loading === true ? <div className="d-flex justify-content-center"><div className="spinner-border " role="status" style={{'width': '3rem', 'height': '3rem'}}>
+        <span className="visually-hidden">Loading...</span></div><h4 className='ms-3'>{operation}</h4>
+      </div> :<div></div>}
       <div className='mt-5'><h2 className='text-center'>Issue List</h2></div>
-      {issues.length > 0 ? <IssueTable issues={issues} handleEdit={handleEditIssue} delete={handleDelete} updateStart={updateStart} updateStatus={handleUpdateStatus} handleStatus ={handleStatus}/> : <div className='mt-2 mb-6'><h3 className='text-center'>No issue added yet.</h3></div>}
 
+      { issues.length > 0 ? <IssueTable issues={issues} handleEdit={handleEditIssue} delete={handleDelete} updateStart={updateStart} updateStatus={handleUpdateStatus} handleStatus={handleStatus} /> : <div className='mt-2 mb-6'><h3 className='text-center'>No issue added yet.</h3></div>}
+      <div id="bottomElement" className='m-5'></div>
 
     </div>
   )
--- src/components/IssueTable.js
+++ src/components/IssueTable.js
@@ -3,45 +3,44 @@ import React from 'react'
 const IssueTable = (props) => {
   return (
     <div className='container mt-1 mb-5'>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Name</th>
-              <th scope="col">Email Id</th>
-              <th scope="col">Issue Type</th>
-              <th scope="col">Description</th>
-              <th scope="col">Status</th>
-              <th scope="col">Edit/Delete</th>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email Id</th>
+            <th scope="col">Issue Type</th>
+            <th scope="col">Description</th>
+            <th scope="col">Status</th>
+            <th scope="col">Edit/Delete</th>
 
+          </tr>
+        </thead>
+        <tbody>
+          {props.issues.map((issue, index) => (
+            <tr key={index}>
+              <th scope="row" id={issue.email}>{index + 1}</th>
+              <td>{issue.name}</td>
+              <td>{issue.email}</td>
+              <td>{issue.issue}</td>
+              <td>{issue.description}</td>
+              <td><button type="button" className={issue.status === false ? 'btn btn-sm btn-primary' : 'btn btn-sm btn-dark'} onClick={() => props.handleStatus(issue.id, { status: issue.status === true ? false : true })}>{issue.status === false ? 'pending' : 'Done'}</button>
+              </td>
+              <td>
+                {issue.status === false?<i className="fas fa-edit me-3 ms-3" title='Edit Issue' onClick={() => props.updateStatus(issue.id,issue.name, issue.email, issue.description, issue.issue)}></i>: <div></div>}
+
+
+                <i className="fas fa-trash " title='Delete Issue' onClick={() => props.delete(issue.id)}></i>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {props.issues.map((issue, index) => (
-              <tr key={index}>
-                <th scope="row">{index + 1}</th>
-                <td>{issue.name}</td>
-                <td>{issue.email}</td>
-                <td>{issue.issue}</td>
-                <td>{issue.description}</td>
-                <td><button type="button" className={issue.status === false ? 'btn btn-sm btn-primary' : 'btn btn-sm btn-dark'}onClick={()=> props.handleStatus(issue.email, {status: issue.status===true?false:true})}>{issue.status === false ? 'pending' : 'Done'}</button>
-</td>
-                {/* <td><i onClick={()=>issue.status === false ? 'done' : 'pending'}>{issue.status === false ? 'pending' : 'Done'}</i></td> */}
-                <td>
-                  <i className="fas fa-edit me-3 ms-3" title='Edit Issue' onClick={()=> props.updateStatus(issue.name, issue.email, issue.description, issue.issue)}></i>
+          ))}
+
+        </tbody>
+      </table>
 
 
-                  <i className="fas fa-trash " title='Delete Issue' onClick={()=>props.delete(issue.email)}></i>
-                </td>
-              </tr>
-            ))}
 
-          </tbody>
-        </table>
-
-        
-
-      </div>
+    </div>
   )
 }
 
